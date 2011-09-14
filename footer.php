f2ad7be3e7486fe90fb6860469e47bda010cfccb
ramen <dave@ramenlabs.com>
2011-09-14 09:58:21 -0700

    Add footer links

--- footer.php
+++ footer.php
@@ -25,8 +25,39 @@
 			?>
 
 			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+				<div class="row">
+					<div class="span2 columns offset2">
+						<a href="http://code.google.com/u/dave.benjamin/">googlecode</a>
+					</div>
+
+					<div class="span2 columns">
+						<a href="http://github.com/ramen/">github</a>
+					</div>
+
+					<div class="span2 columns">
+						<a href="http://twitter.com/ramenlabs/">twitter</a>
+					</div>
+
+					<div class="span2 columns">
+						<a href="http://facebook.com/ramenlabs/">facebook</a>
+					</div>
+
+					<div class="span2 columns">
+						<a href="http://www.linkedin.com/in/ramenlabs">linkedin</a>
+					</div>
+
+					<div class="span2 columns">
+						<a href="http://www.flickr.com/photos/81209814@N00/">flickr</a>
+					</div>
+
+					<div class="span2 columns">
+						<a href="http://reddit.com/user/ramen/">reddit</a>
+					</div>
+
+					<div class="span2 columns" style="text-align: right;">
+						<a href="http://spoomusic.com/">spoomusic</a>
+					</div>
+				</div>
 			</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
